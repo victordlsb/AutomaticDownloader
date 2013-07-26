b1a06bc732b8be6d206d61af4c12d9c338e13e9e
Victor de los Santos Bernad <vis.bcn@gmail>
2013-07-26 03:14:07 +0200

    Reading links working
    Reading links (pdfs only right now) working. TODO next: download those
    links automatically

--- popup.js
+++ popup.js
@@ -1,28 +1,48 @@
 
 function retrieveFilesURLS(url){
 	
-	var doc = document.implementation.createDocument(url, 'html', null);
-	var links = doc.links;
-	document.write(links.length);
-	var arr;
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", url, false);
+	xhr.send();
+	var doc;
+	if(xhr.responseText !== null){
+		var parser = new DOMParser();
+		doc = parser.parseFromString(xhr.responseText, "text/xml");
+	} else if(xhr.responseXML !== null){
+		doc = xhr.responseXML;
+	}
+	console.log(doc);
+	var links = doc.getElementsByTagName("a");
+	var files = new Array();
+	var j = 0;
 	for(var i=0;i<links.length;i++){
-	// Now only detecting pdfs. Using MIME-types to do so
-	// TODO: selecting different MIME-TYPES http://www.freeformatter.com/mime-types-list.html
-		if(links[i].type="application/pdf"){
-			arr.push(links[i]);
-			
+	//Implement the .pdf as an array of the file extension that can be read.
+		if(links[i].href.search(".pdf")===links[i].href.length-4){
+			files[j]=links[i];
+			j++;
 		}
 	}
-	return arr;
-	
+	return files;
 }
+
+
 
 function printURLs(){
-	//document.write("hello world");
-	var arr = retrieveFilesURLS('http://blogs.adobe.com/pdfdevjunkie/web_designers_guide');
-	//document.write(arr);
+//	document.write("hello world");
+	var files = retrieveFilesURLS('http://www.dcs.bbk.ac.uk/courses/msccs/');
+	downloadFiles(files);
 }
 
+function downloadFiles(files){
+	for(var i=0;i<files.length;i++){
+		chrome.downloads.download({url: files[i]},
+			function(id){}
+		);
+	}
+}
+	
 
-document.addEventListener('DOMContentLoaded', function () {printURLs();});
+document.addEventListener('DOMContentLoaded', function () {
+	printURLs();
+});
 
