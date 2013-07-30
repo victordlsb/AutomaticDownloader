313772fd8aecaeb5b470185783535cea33ab81f9
Victor de los Santos Bernad <vis.bcn@gmail>
2013-07-31 01:22:29 +0200

    Function PrintOneURL created.
    This way it is easier to add new websites without reloading everything

--- popup.js
+++ popup.js
@@ -8,46 +8,25 @@
 //printURLs receives an array of TrackedWebsites and print the urls on the main popup
 //It prints also the button to download the files from that website
 function printURLs(websites){
-	var i;
-	var elem = document.getElementById("websitesList");
-	console.log(websites);
-	websites.forEach(function(website){
-		var node = document.createElement("li");
-		var url = document.createTextNode(website.get_URL);
-		var downloadButton = document.createElement('input');
-		downloadButton.setAttribute('type','button');
-		downloadButton.setAttribute('name','down'+i);
-		downloadButton.setAttribute('value','DL');
-		downloadButton.onclick = function(e) {clickDownloadFiles(url);};
-		//downloadButton.setAttribute("onclick","clickDownloadFiles(url)");
-		console.log(url);
-		//downloadButton.addEventListener("onclick",clickDownloadFiles(websites[i].get_Url),false);
-		node.appendChild(url);
-		node.appendChild(downloadButton);
-		elem.appendChild(node);
-	});
+	websites.forEach(PrintOneURL);
 }
 	
-	/*
-	for (i = 0; i<websites.length; i++){ 	
-		var node = document.createElement("li");
-		var url = document.createTextNode(websites[i].get_URL);
-		var downloadButton = document.createElement('input');
-		downloadButton.setAttribute('type','button');
-		downloadButton.setAttribute('name','down'+i);
-		downloadButton.setAttribute('value','DL');
-		downloadButton.onclick = function(e) {clickDownloadFiles(url);};
-		//downloadButton.setAttribute("onclick","clickDownloadFiles(url)");
-		console.log(url);
-		//downloadButton.addEventListener("onclick",clickDownloadFiles(websites[i].get_Url),false);
-		node.appendChild(url);
-		node.appendChild(downloadButton);
-		elem.appendChild(node);
-	}*/
 
-
-
-
+function PrintOneURL(website,index){
+    console.log(website);
+	var elem = document.getElementById("websitesList");
+	var node = document.createElement("li");
+	var url = document.createTextNode(website.get_URL);
+	var downloadButton = document.createElement('input');
+	downloadButton.setAttribute('type','button');
+	downloadButton.setAttribute('name','down'+index);
+	downloadButton.setAttribute('value','DL');
+	downloadButton.onclick = function() {clickDownloadFiles(url);};
+	console.log(url);
+	node.appendChild(url);
+	node.appendChild(downloadButton);
+	elem.appendChild(node);
+}
 
 function clickDownloadFiles(website){
 	console.log(website);
@@ -56,32 +35,21 @@ function clickDownloadFiles(website){
 }
 
 
-//This function loads everything
 
-/*Test websites 
-'http://www.dcs.bbk.ac.uk/courses/'
-'http://www.dcs.bbk.ac.uk/courses/msccs/'
-'http://www.dcs.bbk.ac.uk/courses/mscinfo/'
-*/
-
-
+//For test purposes
+function removeAndReloadWebsites(){
+	localStorage.removeItem(websites);
+	websites.push(trackedWebsite('http://www.dcs.bbk.ac.uk/courses/'));
+	websites.push(trackedWebsite('http://www.dcs.bbk.ac.uk/courses/msccs/'));
+	websites.push(trackedWebsite('http://www.dcs.bbk.ac.uk/courses/mscinfo/'));
+	localStorage.websites=JSON.stringify(websites);
+}
 
 
 function main(){
 	var websites = new Array();
-//	localStorage.removeItem(websites);
-//  websites.push(trackedWebsite('http://www.dcs.bbk.ac.uk/courses/'));
-//	websites.push(trackedWebsite('http://www.dcs.bbk.ac.uk/courses/msccs/'));
-//	websites.push(trackedWebsite('http://www.dcs.bbk.ac.uk/courses/mscinfo/'));
-//	localStorage.websites=JSON.stringify(websites);
 	websites=JSON.parse(localStorage.websites);
 	printURLs(websites);
-	
-	
-	//console.log(retrieveFilesURLs('http://www.dcs.bbk.ac.uk/courses/msccs/'));
-	//console.log(retrieveFilesURLs('http://www.dcs.bbk.ac.uk/courses/'));
-	//console.log(retrieveFilesURLs('http://www.dcs.bbk.ac.uk/courses/mscinfo'));
-	//setDownloadButtons(websites);
 }
 
 
