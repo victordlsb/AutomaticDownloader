a1ae94a8714f75acc8ee0092ac051650cfcb986f
Victor de los Santos Bernad <vis.bcn@gmail>
2013-09-03 04:59:31 +0200

    Monthly algorithm created. Release candidate

--- background.js
+++ background.js
@@ -3,6 +3,7 @@ backgroundInit();
 function backgroundInit(){
 	websites = getAllWebsites();
 	websites.forEach(setAlarms);	
+	chrome.alarms.getAll(function (alarms){ console.log(alarms);});
 }
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
@@ -34,6 +35,7 @@ function downloadOrStore(website){
 
 function setAlarms(website){
 	var schedule = website.schedule;
+	schedule.lastCheck = new Date(schedule.lastCheck);
 	var type = schedule.typeSched;
 	var check = true;
 	//This will make a check if it has never done one on that website 
@@ -47,7 +49,7 @@ function setAlarms(website){
 		case "hourly":
 			
 			if(check){
-				if(schedule.lastCheck.setHour(schedule.lastCheck.getHour() +1) < new Date())
+				if(schedule.lastCheck.setHours(schedule.lastCheck.getHours() +1) < new Date())
 					downloadOrStore(website);
 			}
 			
@@ -57,7 +59,7 @@ function setAlarms(website){
 			} else {
 				nextCheck.setHours(nextCheck.getHours()+1,schedule.atMin);
 			}
-			chrome.alarms.create(JSON.stringify(website),{when:nextCheck,periodInMinutes:60});
+			chrome.alarms.create(JSON.stringify(website),{when:nextCheck.getTime(),periodInMinutes:60});
 			break;
 		
 		case "daily":
@@ -72,7 +74,7 @@ function setAlarms(website){
 			} 
 			nextCheck.setHours(schedule.atHour);
 			nextCheck.setMinutes(schedule.atMin);
-			chrome.alarms.create(JSON.stringify(website),{when:nextCheck,periodInMinutes:1440});
+			chrome.alarms.create(JSON.stringify(website),{when:nextCheck.getTime(),periodInMinutes:1440});
 			break;
 		
 		case "weekly":
@@ -103,11 +105,38 @@ function setAlarms(website){
 			}
 			nextCheck.setHours(schedule.atHour);
 			nextCheck.setMinutes(schedule.atMin);
-			chrome.alarms.create(JSON.stringify(website),{when:nextCheck,periodInMinutes:10080});
+			chrome.alarms.create(JSON.stringify(website),{when:nextCheck.getTime(),periodInMinutes:10080});
 			break;
 		
-	}
+		case "monthly":
 			
+			var nextCheck = new Date();
+			if(check){
+				if(schedule.lastCheck.getYear() < nextCheck.getYear() && ((nextCheck.getMonth !== 0) || schedule.lastCheck !== 11))	{
+					downloadOrStore(website);
+				} else if (schedule.lastCheck.getMonth()+1 < nextCheck.getMonth()){
+					downloadOrStore(website);
+				} else if (schedule.lastCheck.setMonth(schedule.lastCheck.getMonth()+1)  < nextCheck){
+					downloadOrStore(website);
+				}
+			}
 			
+			if(nextCheck.getDate() > schedule.atDay){
+				nextCheck.setMonth(nextCheck.getMonth()+1);
+			}
+			if(schedule.atDay == 29 || schedule.atDay ==30 || schedule.atDay == 31){
+				if(nextCheck.getMonth() === 1 || nextCheck.getMonth() ===3 || nextCheck.getMonth() === 5 || nextCheck.getMonth() === 8 || nextCheck.getMonth() ===10){
+					nextCheck.setDate(1);
+					nextCheck.setMonth(nextCheck.getMonth() + 1);
+				}
+			} else {
+				nextCheck.setDate(schedule.atDay);
+			}
+			nextCheck.setHours(schedule.atHour);
+			nextCheck.setMinutes(schedule.atMin);
+						
+			//For this no repeating alarm is created as it is assumed that the browser will be closed at some point in one month
+			chrome.alarms.create(JSON.stringify(website),{when:nextCheck.getTime()});
 			
+	}			
 }
